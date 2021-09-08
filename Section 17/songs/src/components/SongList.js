4630dc7eab58fcfaac44642dba169ee8a79291a3
Júlia Acosta <juliagabriela98@gmail.com>
2021-09-08 19:06:56 -0300

    add my solution to song list section 17

--- Section 17/songs/src/components/SongList.js
+++ Section 17/songs/src/components/SongList.js
@@ -1,35 +1,71 @@
-import { Component } from "react";
+// import { Component } from "react";
 import { connect } from "react-redux";
 import { selectSong } from "../actions";
 
-class SongList extends Component {
-    renderList() {
-        return this.props.songs.map((song) => {
-            return (
-                <div className="item" key={song.title}>
-                    <div className="right floated content">
-                        <button 
-                            className="ui button primary"
-                            onClick={() => this.props.selectSong(song)}>
+// My version
+const SongList = ({ songs, selectedSong, selectSong }) => {
+    return (
+        <div className="ui divided list">
+            {songs.map((song) => (
+                    <div className="item" key={song.title}>
+                        <div className="right floated content">
+                            <button
+                                className="ui button primary"
+                                style={{background: selectedSong?.title === song.title ? "#ab47bc" : null}}
+                                onClick={() => selectSong(song)}
+                            >
                                 Select
-                        </button>
+                            </button>
+                        </div>
+
+                        <div className="content">{song.title}</div>
                     </div>
-
-                    <div className="content">{song.title}</div>
-                </div>
-            );
-        });
-    }
-
-    render() {
-        return <div className="ui divided list"> {this.renderList()} </div>;
-    }
-}
+                ))}
+        </div>
+    );
+};
 
 const mapStateToProps = (state) => {
-    return { songs: state.songs };
+    return { songs: state.songs,
+        selectedSong: state.selectedSong };
 };
 
 export default connect(mapStateToProps, {
     selectSong,
 })(SongList);
+
+
+// Course version
+
+// class SongList extends Component {
+//     renderList() {
+//         return this.props.songs.map((song) => {
+//             return (
+//                 <div className="item" key={song.title}>
+//                     <div className="right floated content">
+//                         <button
+//                             className="ui button primary"
+//                             onClick={() => this.props.selectSong(song)}>
+//                                 Select
+//                         </button>
+//                     </div>
+
+//                     <div className="content">{song.title}</div>
+//                 </div>
+//             );
+//         });
+//     }
+
+//     render() {
+//         return <div className="ui divided list"> {this.renderList()} </div>;
+//     }
+// }
+
+// const mapStateToProps = (state) => {
+//     return { songs: state.songs };
+// };
+
+// export default connect(mapStateToProps, {
+//     selectSong,
+// })(SongList);
+
